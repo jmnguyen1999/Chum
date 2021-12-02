3991f3b8e1354c3bf63861ef7017ba7f9b31c61e
Josephine <jmnguyen1999@gmail.com>
2021-12-02 10:21:00 -0800

    Adding splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:chum/add_dialog.dart';
 import 'package:chum/models/items/reminder.dart';
 import 'package:chum/models/items/task.dart';
@@ -22,10 +24,59 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Chum'),
+      home: SplashPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
+//Purpose: SplashScreen to display
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+                HomePage(title: 'Chum',)
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFFFFFFF),
+              Color(0xFFD5F3FE),
+              //Color(0xFF66D3FA),
+              Color(0xFF3C99DC),
+              Color(0xFF2565AE)
+              //Color(0xFF2565AE),
+            ],
+          )
+      ),
+      child: Container(
+        alignment: Alignment.center,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.asset('assets/app_logo.png', height:150.0, width:150.0)
+        )
+      ),
+    );
+  }
+}
+
+
 
 
 class HomePage extends StatefulWidget {
